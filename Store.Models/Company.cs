f893d495514cb928e42f5a8992f358486f559ffb
Kiarash Ng <kiarashng99@gmail.com>
2019-06-23 12:27:04 +0430

    Added Models

---
+++ Store.Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    class Company
+    {
+        public int Id { get; set; }
+        public string PersianName { get; set; }
+        public string EnglishName { get; set; }
+        public int? CountryId { get; set; }
+        public virtual Country Country { get; set; }
+    }
+}
